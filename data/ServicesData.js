4b2ab5083f2a91f1d73818dd5c05fa524d0d59e2
bishwajitsarkar99 <bishwajitsarkar99@gmail.com>
2025-11-05 19:31:08 +0600

    updated

--- data/ServicesData.js
+++ data/ServicesData.js
@@ -1,26 +1,26 @@
 export const ServicesData = [
     {
-        url: 'https://bishwajitsarkar99.github.io/portfolio/images/ecommerce.png',
+        url: '',
         title: 'E-Commerce',
         body: 'Industrial Equipment E-Commerce Website.'
     },
     {
-        url: 'https://bishwajitsarkar99.github.io/portfolio/images/animal.png',
+        url: '',
         title: 'Animal E-Commerce',
         body: 'Veterinary Medicine and Farming Equipment Website.'
     },
     {
-        url: 'https://bishwajitsarkar99.github.io/portfolio/images/animal-medicine.png',
+        url: '',
         title: 'Animal Medicine',
         body: 'Backend for Animal Medicine Management.'
     },
     {
-        url: 'https://bishwajitsarkar99.github.io/portfolio/images/ecommerce.png',
+        url: '',
         title: 'Medicine E-Commerce',
         body: 'Backend for Medicine E-Commerce Platform.'
     },
     {
-        url: 'https://bishwajitsarkar99.github.io/portfolio/images/supplier.png',
+        url: '',
         title: 'Supplier Management',
         body: 'Backend for Supplier Management System.'
     }
