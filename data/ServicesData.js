f814354d32b9459cd15c3afdcd2b2f616b437dca
bishwajitsarkar99 <bishwajitsarkar99@gmail.com>
2025-11-10 06:42:11 +0600

    updated

--- data/ServicesData.js
+++ data/ServicesData.js
@@ -1,23 +1,18 @@
 export const ServicesData = [
     {
         url: '',
-        title: 'E-Commerce',
-        body: 'Industrial Equipment E-Commerce Website.'
+        title: 'Industrial Equipment System',
+        body: 'Forntend for Industrial Equipment E-Commerce Website.'
     },
     {
         url: '',
-        title: 'Animal E-Commerce',
-        body: 'Veterinary Medicine and Farming Equipment Website.'
+        title: 'Animal Framing System',
+        body: 'Forntend for Veterinary Medicine and Farming Equipment Website.'
     },
     {
         url: '',
         title: 'Animal Medicine',
         body: 'Backend for Animal Medicine Management.'
-    },
-    {
-        url: '',
-        title: 'Medicine E-Commerce',
-        body: 'Backend for Medicine E-Commerce Platform.'
     },
     {
         url: '',
